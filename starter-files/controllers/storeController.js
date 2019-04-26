f54831133758be9401f548a45a4807a88b6232fe
Sarah Ney <sarah.ney@anthroware.com>
2019-04-26 09:12:31 -0400

    setting up the add/create store ability- added a form layout and a new controller for posting the data to the store

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,7 +1,15 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = "Sarah";
-    next();
-}
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     res.render('index');
-}
+};
+
+exports.addStore = (req, res) => {
+    res.render('editStore', { title: 'Add store'});
+};
+
+exports.createStore = (req, res) => {
+    const store = new Store(req.body);
+    store.save();
+};
