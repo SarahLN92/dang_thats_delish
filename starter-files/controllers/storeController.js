43721bfa4a6d9c35e98e5ef052cd80ce3085b4fc
Sarah Ney <sarah.ney@anthroware.com>
2019-04-29 09:17:31 -0400

    added flashing- aka middleware messages for the createstore page and home

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -3,13 +3,17 @@ const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
     res.render('index');
+    req.flash('error', 'Soemthing Happened');
 };
 
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add store'});
 };
 
-exports.createStore = (req, res) => {
+
+exports.createStore = async (req, res) => {
     const store = new Store(req.body);
-    store.save();
+    await store.save();
+    req.flash('success', `Successfully Created ${store.name} care to leave a review?`);
+    res.redirect('/');
 };
